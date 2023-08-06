53645cfde09d1730f31c3139426516e881b9a7de
yuanfulin333 <141508414+yuanfulin333@users.noreply.github.com>
2023-08-06 15:52:57 +0800

    hhhhss

---
+++ web1/src/main/java/com/yuanfulin/mapper/MapperTest.java
@@ -0,0 +1,12 @@
+package com.yuanfulin.mapper;
+
+/**
+ * ClassName:MapperTest
+ * Package:com.yuanfulin.mapper
+ * Description:
+ *
+ * @DATE:2023/8/6 15:52
+ * @Author:袁福林
+ */
+public interface MapperTest {
+}
